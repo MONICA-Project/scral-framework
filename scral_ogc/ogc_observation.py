3211e995464a36d83698d7d8793b0726f35e77e8
Luca Mannella <luca.mannella@linksfoundation.com>
2019-02-13 14:20:56 +0100

    Bug fix: "encodingType" was not a field of OBSERVATION.

--- scral_ogc/ogc_observation.py
+++ scral_ogc/ogc_observation.py
@@ -3,13 +3,12 @@ class OGCObservation:
         For more info: http://developers.sensorup.com/docs/#observations_post
     """
 
-    def __init__(self, ogc_datastream_id, phenomenon_time, result_time, result, encoding_type="application/vnd.geo+json"):
+    def __init__(self, ogc_datastream_id, phenomenon_time, result, result_time):
         self.id = None  # the id is assigned by the OGC Server
         self.ogc_datastream_id = ogc_datastream_id
         self.phenomenon_time = phenomenon_time
         self.result_time = result_time
         self.result = result
-        self.encoding_type = encoding_type
 
     def set_id(self, observation_id):
         self["id"] = observation_id
@@ -19,4 +18,4 @@ class OGCObservation:
 
     def get_rest_payload(self):
         return {"phenomenonTime": self.phenomenon_time, "resultTime": self.result_time, "result": self.result,
-                "encodingType": self.encoding_type, "Datastream": {"@iot.id": self.ogc_datastream_id}}
+                "Datastream": {"@iot.id": self.ogc_datastream_id}}
