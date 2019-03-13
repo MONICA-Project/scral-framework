e6d1eb8066a6b3ef8361f869ce4a6b728ba422c3
jfoglietti <foglietti@ismb.it>
2019-03-13 16:54:23 +0100

    Started thread pool implementation.

--- sound_level_meter/slm_module.py
+++ sound_level_meter/slm_module.py
@@ -10,6 +10,7 @@
 # SCRAL is distributed under a BSD-style license -- See file LICENSE.md     #
 #                                                                           #
 #############################################################################
+import threading
 
 import requests
 import json
@@ -45,33 +46,46 @@ class SCRALSoundLevelMeter(SCRALModule):
         self._listening_port = int(connection_config_file["REST"]["listening_address"]["port"])
 
         # Get cloud access token by using available credentials
-        self._cloud_token = util.get_server_access_token(URL_SLM_LOGIN, CREDENTIALS, REST_HEADERS, token_prefix="Bearer ")
+        self._cloud_token = util.get_server_access_token(URL_SLM_LOGIN, CREDENTIALS,
+                                                         REST_HEADERS, token_prefix="Bearer ")
 
         self._sequences = []
 
+        self._active_devices = {}
+
         global _slm_module
         _slm_module = self
+
+    def get_active_devices(self):
+        return self._active_devices
+
+    @property
+    def get_cloud_token(self):
+        return self._cloud_token
 
     # noinspection PyMethodOverriding
     def runtime(self):
         """
         This method discovers active SLMs from B&K cloud, registers them as OGC Datastreams into the MONICA
-        server and finally retrieves sound measurements for each device by using parallel querying threads.
+        cloud and finally retrieves sound measurements for each device by using parallel querying threads.
 
         Furthermore, this method deploys an REST endpoint as Flask application based on CherryPy WSGI web server.
         The endpoint will listen for incoming REST requests of the type "sound event detection",
         and will generate corresponding Observations.
         """
-        # ### Start SLM discovery from B&K cloud
+        # Start SLM discovery and Datastream registration
         self.ogc_datastream_registration(URL_SLM_CLOUD, TENANT_ID, SITE_ID)
+
+        # Start thread pool for Observations
+        # self.start_thread_pool(self._active_devices)
 
     def ogc_datastream_registration(self, url, tenant_id, site_id):
         """
         This method discovers active SLMs for a given site_id and registers a new OGC Datastream for
         each couple SLM-ObservedProperty.
-        :param url: B&K server address
-        :param tenant_id: B&K server tenant_id
-        :param site_id: B&K server site_id (pilot specific)
+        :param url: server address
+        :param tenant_id: server tenant_id
+        :param site_id: server site_id (pilot specific)
         """
 
         url_locations = url + "/tenants/" + str(tenant_id) + "/sites/" + str(site_id) + "/locations"
@@ -96,7 +110,6 @@ class SCRALSoundLevelMeter(SCRALModule):
             locations_id_list.append(location["locationId"])
 
         # Discover active devices from active location ids (assumption: 1 SLM x 1 location_id)
-        active_devices = {}
         for location_id in locations_id_list:
             logging.info("Start discovery for active SLMs in a given site (pilot)")
             url_loc = url_locations + "/" + str(location_id)
@@ -105,24 +118,23 @@ class SCRALSoundLevelMeter(SCRALModule):
             except Exception as ex:
                 logging.error(ex)
 
-            # Get device id, name, description and coordinates assigned to location item
+            # Get device id, name, description and coordinates information stored in location item
             location_item = r.json()
-            location_assigned_device_id = location_item["assignedDevices"][0]["deviceId"]
-            location_assigned_device_name = location_item["assignedDevices"][0]["name"]
-            location_assigned_device_description = location_item["assignedDevices"][0]["description"]
+            device_id = location_item["assignedDevices"][0]["deviceId"]
+            device_name = location_item["assignedDevices"][0]["name"]
+            device_description = location_item["assignedDevices"][0]["description"]
             location_coordinates = location_item["geoLocation"]
-            logging.debug("SLM id: "+str(location_assigned_device_id)+" / SLM name: "+location_assigned_device_name)
+            logging.debug("SLM id: "+str(device_id)+" / SLM name: "+device_name)
 
             # Build active devices catalog
-            active_devices[location_assigned_device_id] = {}
-            active_devices[location_assigned_device_id]["coordinates"] = location_coordinates["coordinates"]
-            active_devices[location_assigned_device_id]["name"] = location_assigned_device_name
-            active_devices[location_assigned_device_id]["description"] = location_assigned_device_description
+            self._active_devices[device_id] = {}
+            self._active_devices[device_id]["coordinates"] = location_coordinates["coordinates"]
+            self._active_devices[device_id]["name"] = device_name
+            self._active_devices[device_id]["description"] = device_description
 
             url_sequences = url + "/tenants/" + str(tenant_id) + "/sites/" + str(site_id) + "/locations" + str(
                 location_id) + "/sequences"
-
-        logging.debug("ACTIVE SLMs catalog: " + str(active_devices))
+            self._active_devices[device_id]["location_sequences"] = url_sequences
 
         # Collect OGC information needed to build Datastreams payload
         thing = self._ogc_config.get_thing()
@@ -137,9 +149,8 @@ class SCRALSoundLevelMeter(SCRALModule):
 
         # Start OGC Datastreams registration
         logging.debug("Start OGC DATASTREAMs registration")
-        for device_id, values in active_devices.items():
-            logging.debug("Device id: "+str(device_id))
-            logging.debug("Device values: "+str(values))
+        # Iterate over active devices
+        for device_id, values in self._active_devices.items():
             device_name = values["name"]
             device_coordinates = values["coordinates"]
             device_description = values["description"]
@@ -150,12 +161,11 @@ class SCRALSoundLevelMeter(SCRALModule):
             else:
                 self._resource_catalog[device_name] = {}
 
-                # Iterate over SLM assigned ObservedProperties
+                # Iterate over ObservedProperties
                 for ogc_property in properties_list:
                     property_id = ogc_property.get_id()
                     property_name = ogc_property.get_name()
                     property_definition = {"definition": ogc_property.get_definition()}
-                    logging.debug("Device property: " + str(property_name))
                     datastream_name = thing_name + "/" + sensor_name + "/" + property_name + "/" + device_name
                     description = device_description
                     datastream = OGCDatastream(name=datastream_name, description=description,
@@ -170,3 +180,50 @@ class SCRALSoundLevelMeter(SCRALModule):
 
                     # Store device/property information in local resource catalog
                     self._resource_catalog[device_name][property_name] = datastream_id
+
+                    logging.debug("Added Datastream: " + str(datastream_id) + " to the resource catalog for device: "
+                                  + device_name + " and property: " + property_name)
+
+    def start_thread_pool(self, active_devices):
+        thread_pool = []
+        thread_id = 1
+
+        for device_id, values in self._active_devices.items():
+            thread = self.MyThread(thread_id, "Thread-" + str(thread_id), values["name"], values["location_sequences"])
+            thread.start()
+            thread_pool.append(thread)
+            thread_id += 1
+
+        for thread in thread_pool:
+            thread.join
+
+        logging.debug("Exiting Main Thread")
+
+    class MyThread(threading.Thread):
+        def __init__(self, thread_id, thread_name, device_id, url_sequences):
+            threading.Thread.__init__(self)
+            self.thread_name = thread_name
+            self.thread_id = thread_id
+            self.device_id = device_id
+            self.url_sequences = url_sequences
+
+        def run(self):
+            logging.debug("Starting Thread: " + self.thread_name)
+            self.fetch_sequences(self.thread_id, self.thread_name, self.device_id, self.url_sequences)
+            logging.debug("Exiting " + self.thread_name)
+
+        @staticmethod
+        def fetch_sequences(thread_id, thread_name, device_id, url_sequences):
+
+            r = None
+            try:
+                r = requests.get(url_sequences, headers=_slm_module.get_cloud_token)
+            except Exception as ex:
+                logging.error(ex)
+            if r is None:
+                raise ConnectionError(
+                    "Connection status: " + str(r.status_code) + "\nImpossible to establish a connection with "
+                    + url_sequences)
+            else:
+                for sequence in r.json()['value']:
+                    logging.debug(thread_name + " sequence: " + json.dumps(sequence))
