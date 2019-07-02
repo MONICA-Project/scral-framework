c5b74f09e46cefd22dc049fe9d93619d2c3ad33a
Luca Mannella <luca.mannella@linksfoundation.com>
2019-07-02 18:04:06 +0200

    Phonometer module now correctly retrieve all phonometers on platform.

--- phonometer/constants.py
+++ phonometer/constants.py
@@ -16,9 +16,9 @@
 """
 
 URL_CLOUD = "https://api.smartdatanet.it/api"
-URL_TENANT = "https://api.smartdatanet.it/metadataapi/api/v02/search?tenant=cittato_rumore&top=1000"
+URL_TENANT = "https://api.smartdatanet.it/metadataapi/api/v02/search?tenant=cittato_rumore"
 
-ACTIVE_DEVICES = ["ds_S_01_1928", "ds_S_02_1926", "ds_S_03_1927", "ds_S_05_1932", "ds_S_06_1931"]
+ACTIVE_DEVICES = ["ds_S_07_1933"] # "ds_S_01_1928", "ds_S_02_1926", "ds_S_03_1927", "ds_S_05_1932", "ds_S_06_1931"]
 
 FREQ_INTERVALS = ["16", "20", "25", "31_5", "40", "50", "63", "80", "100", "125", "160", "200",
                   "250", "315", "400", "500", "630", "800", "1000", "1250", "1600", "2000", "2500",
@@ -28,8 +28,10 @@ FILTER_SDN_1 = "Measures?%20&$format=json&$filter=time%20gt%20datetimeoffset%27"
 FILTER_SDN_2 = "%27%20and%20time%20le%20datetimeoffset%27"
 FILTER_SDN_3 = "%27&$orderby=time%20asc&$top=50"
 
-LAEQ_KEY = "LAeq"
-SPECTRA_KEY = "CPBLZeq"
-
+# Timing
 UPDATE_INTERVAL = 15
 RETRY_INTERVAL = 120
+
+# Keys
+LAEQ_KEY = "LAeq"
+SPECTRA_KEY = "CPBLZeq"
--- phonometer/phonometer_module.py
+++ phonometer/phonometer_module.py
@@ -26,7 +26,7 @@ from scral_module.constants import REST_HEADERS, CATALOG_FILENAME
 from scral_ogc import OGCDatastream
 
 from microphone.microphone_module import SCRALMicrophone
-from microphone.constants import SEQUENCES_KEY
+from microphone.constants import SEQUENCES_KEY, NAME_KEY
 
 from phonometer.constants import URL_TENANT, ACTIVE_DEVICES, FILTER_SDN_1, URL_CLOUD, UPDATE_INTERVAL, LAEQ_KEY, \
     SPECTRA_KEY, RETRY_INTERVAL, FREQ_INTERVALS, FILTER_SDN_2, FILTER_SDN_3
@@ -62,48 +62,60 @@ class SCRALPhonometer(SCRALMicrophone):
 
         :param url: Target server address.
         """
-        r = None
-        try:
-            r = requests.get(url)
-        except Exception as ex:
-            logging.error(ex)
 
-        if not r or not r.ok:
-            raise ConnectionError(
-                "Connection status: " + str(r.status_code) + "\nImpossible to establish a connection with " + url)
-
-        # Start OGC Datastreams registration
         logging.info("\n\n--- Start OGC DATASTREAMs registration ---\n")
-        phonometers = r.json()["metadata"]
+        count = 0
+        i = 1
+        while True:
+            r = None
+            query_url = url + "&start="+str(count)
+            try:
+                r = requests.get(query_url)
+            except Exception as ex:
+                logging.error(ex)
 
-        logging.debug("Phonometers retrieved:")
-        for phono in phonometers:
-            logging.debug(phono["stream"]["smartobject"]["name"])
-            logging.debug(phono["stream"]["smartobject"]["code"]+"\n")
+            if not r or not r.ok:
+                raise ConnectionError(
+                    "Connection status: "+str(r.status_code)+"\nImpossible to establish a connection with "+query_url)
 
-        # Iterate over active devices
-        for phono in phonometers:
-            if phono["dataset"]["code"] in ACTIVE_DEVICES:
-                device_id = phono["dataset"]["code"]
-                device_name = phono["name"]
-                device_coordinates = (phono["longitude"], phono["latitude"])
-                device_description = phono["description"]
+            payload = r.json()
+            phonometers = payload["metadata"]
 
-                # Check whether device has been already registered
-                if device_id in self._resource_catalog:
-                    logging.debug("Device: " + device_name + " already registered with id: " + device_id)
-                else:
-                    self._resource_catalog[device_id] = {}
-                    self._active_devices[device_id] = {}
-                    # Iterate over ObservedProperties
-                    for ogc_property in self._ogc_config.get_observed_properties():
-                        self._new_datastream(
-                            ogc_property, device_id, device_coordinates, device_description)
+            logging.debug("\n\nPhonometers retrieved (page "+str(i)+"):\n")
+            for phono in phonometers:
+                logging.debug(phono["stream"]["smartobject"]["name"])
+                logging.debug(phono["stream"]["smartobject"]["code"]+"\n")
 
-                url_sequence = URL_CLOUD + '/' + device_id + '/' + FILTER_SDN_1
-                self._active_devices[device_id][SEQUENCES_KEY] = url_sequence
+            # OGC DATASTREAM Registration ONLY of active devices
+            for phono in phonometers:
+                if phono["dataset"]["code"] in ACTIVE_DEVICES:
+                    device_id = phono["dataset"]["code"]
+                    device_name = phono["name"]
+                    device_coordinates = (phono["longitude"], phono["latitude"])
+                    device_description = phono["description"]
 
-        self.update_file_catalog()
+                    # Check whether device has been already registered
+                    if device_id in self._resource_catalog:
+                        logging.debug("Device: " + device_name + " already registered with id: " + device_id)
+                    else:
+                        self._resource_catalog[device_id] = {}
+                        self._active_devices[device_id] = {}
+                        # Iterate over ObservedProperties
+                        for ogc_property in self._ogc_config.get_observed_properties():
+                            self._new_datastream(
+                                ogc_property, device_id, device_coordinates, device_description)
+
+                        url_sequence = URL_CLOUD + '/' + device_id + '/' + FILTER_SDN_1
+                        self._active_devices[device_id][SEQUENCES_KEY] = url_sequence
+                        self._active_devices[device_id][NAME_KEY] = device_name
+
+            total_count = payload["totalCount"]
+            count += payload["count"]
+            i += 1
+            if count >= total_count:
+                break
+
+        # self.update_file_catalog()
         logging.info("--- End of OGC DATASTREAMs registration ---\n")
 
     def _new_datastream(self, ogc_property, device_id, device_coordinates, device_description):
@@ -153,11 +165,14 @@ class SCRALPhonometer(SCRALMicrophone):
         def __init__(self, thread_id, thread_name, device_id, url_sequence, phonometer_module):
             super().__init__()
 
+            # thread_debug_name = "("+str(thread_id)+")"
+            thread_debug_name = "(" + thread_name + ")"
+
             # Init Thread logger
-            self._logger = util.init_mirrored_logger("("+str(thread_id)+")", logging.DEBUG)
+            self._logger = util.init_mirrored_logger(thread_debug_name, logging.DEBUG)
 
             # Enable log storage in file
-            # self._logger = util.init_mirrored_logger(str(thread_id), logging.DEBUG, "mic_"+str(thread_id)+".log")
+            # self._logger = util.init_mirrored_logger(thread_debug_name, logging.DEBUG, "mic_"+thread_debug_name+".log")
 
             self._thread_name = thread_name
             self._thread_id = thread_id
