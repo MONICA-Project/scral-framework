3d03fbe0e5738bd491ed1a3a212748590d421739
Luca Mannella <luca.mannella@linksfoundation.com>
2019-09-18 17:39:00 +0200

    Quickstart deployment implementation of some SCRAL modules.
    - Implemented modules:
    env_sensor_onem2m, gps_tracker_poll, gps_tracker_rest, phonometer, sfn, smart_glasses
    - Modules that will be implemented:
    slm, wristband, wristband_debug, wristband_mqtt
    - Modules that should be tested by Jacopo:
    wristband (nginx + uwsgi)
    - small code refactoring and uniformity between modules

--- gps_tracker_poll/setup.py
+++ gps_tracker_poll/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
+setup(name='scral_ogc', version='1.0', packages=find_packages())
 setup(name='scral_module', version='1.0', packages=find_packages(),
       install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser'])
-setup(name='scral_ogc', version='1.0', packages=find_packages(),
+setup(name='gps_tracker', version='1.0', packages=find_packages(),
       install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser'])
 setup(name='gps_tracker_poll', version='1.0', packages=find_packages(),
       install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser'])
