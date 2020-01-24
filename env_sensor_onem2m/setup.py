090d96282d9dc4579f5be05eaa17770f53f92f8b
Luca Mannella <luca.mannella@linksfoundation.com>
2020-01-24 17:28:37 +0100

    Modules that will not be open source are removed.

--- env_sensor_onem2m/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='scral_core', version='1.0', packages=find_packages(),
-      install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser'])
-setup(name='scral_ogc', version='1.0', packages=find_packages(),
-      install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser'])
-setup(name='env_sensor_onem2m', version='1.0', packages=find_packages(),
-      install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser', 'flask', 'cherrypy'])
