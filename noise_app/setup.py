ffeb71a2c439f9e2954c6151fc3a48596bf52b60
Luca Mannella <luca.mannella@linksfoundation.com>
2020-01-24 18:00:51 +0100

    .gitignore file updated with PyCharm files

--- noise_app/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='scral_ogc', version='1.0', packages=find_packages())
-setup(name='scral_core', version='1.0', packages=find_packages(),
-      install_requires=['requests', 'paho-mqtt', 'arrow', 'config_parser', 'flask', 'cherrypy'])
-setup(name='noise_app', version='1.0', packages=find_packages(),
-      install_requires=['requests', 'paho-mqtt', 'arrow', 'jproperties'])
