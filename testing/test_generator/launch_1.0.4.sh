339430200f819020cf558856bc9360d0b1a70b2e
Luca Mannella <luca.mannella@linksfoundation.com>
2020-01-28 17:32:46 +0100

    Removed useless files for open source repository.

--- testing/test_generator/launch_1.0.4.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar wristband_simulation_1.0.4.jar settingsApplication_1.0.4.conf
--- testing/test_generator/launch_old.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar ./wristband_simulation_old.jar ./settingsApplication_old.conf
